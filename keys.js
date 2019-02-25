25f8b69fe8700f1b19847a9882a2fc4def767158
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-02-24 18:23:55 -0600

    added in the key info

--- keys.js
+++ keys.js
@@ -4,3 +4,11 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+// exports.bandsInTownAPI = {
+//   key: process.env.
+// }
+
+exports.omdbiAPI = {
+  key: process.env.OMDBIAPI
+}
